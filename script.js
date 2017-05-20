c9e2ebc1f0a5dfcc64f60556d744d9922f0fcb14
Tobiasz12 <tobiasz63@op.pl>
2017-05-20 09:38:17 +0200

    task_2.4

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-var Movie = React.createClass('Movie', function() {
-	render(){
-		return (
-	      	React.createElement('div', {},
-	       		React.createElement('h2', {}, this.props.movie.name),
-	        	React.createElement('p', {}, this.props.movie.disc)
-	      	)
-	    )
-	}
-})
 
-propTypes: {
-    movie: React.PropTypes.object.isRequired,
-  },
+var Movie = React.createClass({
+ propTypes: {
+  movie: React.PropTypes.object.isRequired,
+ },
+ render: function() {
+  return React.createElement('li', {key: this.props.movie.id},
+    React.createElement(MovieTitle, {title: this.props.movie.title} ),
+    React.createElement(MovieDescription, {desc: this.props.movie.desc}),
+    React.createElement('img', { src: this.props.movie.img })
+  );
+ }
+});
+
+var MovieTitle = React.createClass({
+	 propTypes: {
+     	title: React.PropTypes.string.isRequired,
+ 	},
+	 render: function() {
+	  return React.createElement('h1',{}, movie.title);
+	 }
+});
+
+var MovieDescription = React.createClass({
+	propTypes: {
+    	desc: React.PropTypes.string.isRequired,
+ 	},
+	 render: function() {
+	  return React.createElement('p',{}, movie.desc);
+	 }
+});
+
 
 var movie = {
-	name: 'Predator',
-	disc: 'o kosmitach'
+	id:1,
+	title: 'Piraci z Karaibów',
+	desc: 'film o piratach',
+	img: 'http://1.fwcdn.pl/po/65/42/606542/7785907.6.jpg'
 }
 
-var element = React.createElement(Movie, {movie: movie});
+var element = React.createElement(Movie, {movie:movie});
 ReactDOM.render(element, document.getElementById('app'));
 
